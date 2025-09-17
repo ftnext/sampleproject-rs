a156dd311d3b351b2c957e9d8e86265865a3cf4d
ftnext <takuyafjp+develop@gmail.com>
2025-09-17 23:37:56 +0900

    [feat] sample command by project.scripts

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,20 @@
 use pyo3::prelude::*;
 
 #[pyfunction]
+fn main() {
+    println!("Call your main application code here");
+}
+
+#[pyfunction]
 fn add_one(number: i32) -> i32 {
     number + 1
 }
 
 #[pymodule]
 mod sample {
+    #[pymodule_export]
+    use super::main;
+
     #[pymodule_export]
     use super::add_one;
 }
