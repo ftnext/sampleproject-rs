5f78c85cd9584f4c3cd9093ccebb32da0e96c15d
ftnext <takuyafjp+develop@gmail.com>
2026-09-05 14:37:14 +0900

    refactor: Clean root Cargo.toml; Remove Rust CLI

--- src/main.rs
+++
@@ -1,3 +0,0 @@
-fn main() {
-    println!("Call your main application code here");
-}
--- tests/cli.rs
+++
@@ -1,8 +0,0 @@
-use assert_cmd::Command;
-
-#[test]
-fn test_cli() {
-    let mut cmd = Command::cargo_bin("sample").unwrap();
-    let assert = cmd.assert();
-    assert.success().stdout("Call your main application code here\n");
-}
